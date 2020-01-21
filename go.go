642dcb7a3dc405c71668fa2a290463a4536e880e
Miles Whittaker <mj@whitta.dev>
2020-01-21 13:10:18 -0500

    Updated and split go template

--- go.go
+++ go.go
@@ -8,20 +8,6 @@ import (
 	hl "gitlab.com/mjwhitta/hilighter"
 )
 
-// Helpers begin
-
-func err(msg string) { hl.PrintlnRed("[!] %s", msg) }
-func errx(status int, msg string) {
-	err(msg)
-	os.Exit(status)
-}
-func good(msg string)    { hl.PrintlnGreen("[+] %s", msg) }
-func info(msg string)    { hl.PrintlnWhite("[*] %s", msg) }
-func subinfo(msg string) { hl.PrintlnCyan("[=] %s", msg) }
-func warn(msg string)    { hl.PrintlnYellow("[-] %s", msg) }
-
-// Helpers end
-
 // Exit status
 const (
 	Good             int = 0
